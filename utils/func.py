50a3833e33fbe27b7958454e6207b2bf5c297912
user <ibrahim42babayev@gmail.com>
2023-11-30 23:06:44 +0300

    изменения

--- utils/func.py
+++ utils/func.py
@@ -1,87 +1,96 @@
 import json
+from datetime import date
 
 
 def open_operations():
-    file=open("../operations.json", encoding="utf-8")
+    """Функция считывает приложенный словарь с операциями через библиотеку json"""
+    file = open("../operations.json", encoding="utf-8")
     filename = json.load(file)
 
     return filename
 
 
-data=open_operations()
-
 def is_Executed():
-    new_data=[]
-    for i in range(len(data)):
-        if "from" in data[i]:
-            if data[i]["from"][0]=="V" or data[i]["from"][0]=='M':
-                if len(data[i]["to"])>20:
-                    if "state" in data[i]:
-                        if data[i]["state"]=="EXECUTED":
-                            new_data.append(data[i])
+    """Функция проверяет статус перевода и выводит только те операции,которые выполнены"""
+    new_data = []
+    for i in range(len(open_operations())):
+        # Проверяем наличие данного "откуда",так как в некоторых операциях его может и не быть
+        if "from" in open_operations()[i]:
+            # Проверяем имя карты с которого была произведена оплата. Эта проверка нужна,так как в некоторых операциях поля "откуда" неправильно прописаны
+            if open_operations()[i]["from"][0] == "V" or open_operations()[i]["from"][0] == 'M':
+                # Проверяем длину поля "куда",так как в некоторых операциях там написано просто имя карты или же номер получателя короткий
+                if len(open_operations()[i]["to"]) > 20:
+                    # Проверяем наличия данных о статусе перевода
+                    if "state" in open_operations()[i]:
+                        # Проверяем выполнена ли операция
+                        if open_operations()[i]["state"] == "EXECUTED":
+                            new_data.append(open_operations()[i])
                         else:
                             continue
 
     return new_data
 
 
-new_data=is_Executed()
-
-
-
 def get_executed_dates():
+    """Функция выводит дату из словаря"""
     data_list = []
-    for j in range(len(new_data)):
-        data_list.append(new_data[j]["date"][:10])
-    max_list=[]
+    for j in range(len(is_Executed())):
+        # Выводим только до 10-го символа,так как время оплаты нам не нужно
+        data_list.append(is_Executed()[j]["date"][:10])
+    max_list = []
 
     for _ in range(5):
-        max="0"
+        max = "0"
         for k in range(len(data_list)):
-            if data_list[k]>max:
-                max=data_list[k]
+            # Выводим максимальную дату.То есть дату операции,которая была произведена позднее всех
+            if data_list[k] > max:
+                max = data_list[k]
 
         data_list.remove(max)
         max_list.append(max)
 
     return max_list
 
-max_list=get_executed_dates()
 
 def id_of_maxlist():
+    """Функция выводит все данные с правильными датами для дальнейшого использования"""
     max_list_id = []
-    for p in max_list:
-        for i in range(len(new_data)):
-            if p in new_data[i]["date"][:10]:
-                max_list_id.append(new_data[i])
+    for p in get_executed_dates():
+        for i in range(len(is_Executed())):
+            if p in is_Executed()[i]["date"][:10]:
+                max_list_id.append(is_Executed()[i])
 
     return max_list_id
 
-max_list_id=id_of_maxlist()
-
 
 def correct_data():
-    cd=[]
-    for i in max_list:
-         i=i[8:]+i[4:7]+"-"+i[:4]
-         cd.append(i)
+    """Функция превращает строку в дату через импортированный модуль datetime"""
+    cd = []
+    for i in get_executed_dates():
+        thedate = date.fromisoformat(i)
+        date_formatted = thedate.strftime("%d-%m-%Y ")
+        cd.append(date_formatted)
 
     return cd
 
 
 def senders_card_number():
+    """Функция маскирует номер карты отправителя в формате XXXX XX** **** XXXX"""
     card_numbers = []
-    for i in range(len(max_list_id)):
+    for i in range(len(id_of_maxlist())):
+        # Создаём новый список,чтобы добавить туда номер и тип карты отправителя
         accounts = []
-        for k, v in max_list_id[i].items():
+        for k, v in id_of_maxlist()[i].items():
             if k == "from":
                 accounts.append(v)
+        # Создаём ещё один список,куда добавляем только номер карты отправителя
         number = []
         for j in accounts:
             j.split()
             for m in j:
                 if m.isdigit():
                     number.append(m)
+            # Маскируем номер в нужном формате
             number[6] = "*"
             number[7] = "*"
             number[8] = "*"
@@ -94,13 +103,17 @@ def senders_card_number():
 
     return card_numbers
 
+
 def senders_card_type():
+    """Фукнция выводит тип карты"""
     card_type = []
-    accounts=[]
-    for i in range(len(max_list_id)):
-        for k, v in max_list_id[i].items():
+    # Создаём новый список,чтобы добавить номер и тип карты отправителя
+    accounts = []
+    for i in range(len(id_of_maxlist())):
+        for k, v in id_of_maxlist()[i].items():
             if k == "from":
                 accounts.append(v)
+    # Выводим из этого списка тип карты
     for j in accounts:
         j.split()
         for m in j:
@@ -112,13 +125,16 @@ def senders_card_type():
 
 
 def receivers_card_number():
+    """Функция выводит номер карты получателя в формате,где видны только последние 4 цифры """
     card_numbers = []
-    for i in range(len(max_list_id)):
+    # Создаём новый список и добавляем туда счёт получателя
+    for i in range(len(id_of_maxlist())):
         accounts = []
-        for k, v in max_list_id[i].items():
+        for k, v in id_of_maxlist()[i].items():
             if k == "to":
                 accounts.append(v)
         number = []
+        # Создаём ещё один список и выводим номер получателя в нужном нам формате
         for j in accounts:
             j.split()
             for m in j:
@@ -132,42 +148,31 @@ def receivers_card_number():
     return card_numbers
 
 
-
 def amount():
+    """Функция выводит сумму операции"""
     amounts = []
-    for i in range(len(max_list_id)):
-        for k, v in max_list_id[i]["operationAmount"].items():
+    for i in range(len(id_of_maxlist())):
+        for k, v in id_of_maxlist()[i]["operationAmount"].items():
             if k == "amount":
                 amounts.append(v)
     return amounts
 
 
-
 def currency():
+    """Фукнция выводит валюту операции"""
     currency = []
-    for i in range(len(max_list_id)):
-        for k, v in max_list_id[i]["operationAmount"]["currency"].items():
+    for i in range(len(id_of_maxlist())):
+        for k, v in id_of_maxlist()[i]["operationAmount"]["currency"].items():
             if k == "name":
                 currency.append(v)
     return currency
 
 
-
 def description():
-    description=[]
-    for i in range(len(max_list_id)):
-        for k,v in max_list_id[i].items():
-             if k == "description":
+    """Функция выводит описание типа перевода"""
+    description = []
+    for i in range(len(id_of_maxlist())):
+        for k, v in id_of_maxlist()[i].items():
+            if k == "description":
                 description.append(v)
     return description
-
-
-
-for i in range(5):
-    x=f"{correct_data()[i]} {description()[i]}"
-    y=f"{senders_card_type()[i]}{senders_card_number()[i]} -> Счет {receivers_card_number()[i]}"
-    z=amount()[i] + " " + currency()[i]
-    print(x)
-    print(y)
-    print(z)
-    print()
---
+++ utils/main.py
@@ -0,0 +1,21 @@
+import json
+from datetime import date
+from utils.func import *
+
+
+# for i in range(5):
+#     x = f"{correct_data()[i]} {description()[i]}"
+#     y = f"{senders_card_type()[i]}{senders_card_number()[i]} -> Счет {receivers_card_number()[i]}"
+#     z = amount()[i] + " " + currency()[i]
+#     print(x)
+#     print(y)
+#     print(z)
+#     print()
+def main():
+  for i in range(5):
+    x = f"{correct_data()[i]} {description()[i]}\n{senders_card_type()[i]}{senders_card_number()[i]} -> Счет {receivers_card_number()[i]}\n{amount()[i]}  {currency()[i]}\n"
+    print(x)
+    print()
+
+
+main()
